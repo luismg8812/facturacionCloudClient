a35b7e0afba4b78cfec9047fbfa559c789c3626f
luis miguel gonzalez <luismg8812@hotmail.com>
2020-01-20 21:28:16 -0500

    se agrega desarrollo de orden de trabajo

--- src/app/ot/ot.component.ts
+++ src/app/ot/ot.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit,ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ClienteModel } from '../model/cliente.model';
 import { ClienteService } from '../services/cliente.service';
 import { DocumentoModel } from '../model/documento.model';
 import { DocumentoService } from '../services/documento.service';
 import { DocumentoDetalleService } from '../services/documento-detalle.service';
 import { CalculosService } from '../services/calculos.service';
+import { DocumentoDetalleModel } from '../model/documentoDetalle.model';
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-ot',
@@ -15,17 +18,19 @@ export class OtComponent implements OnInit {
   public nombreUsuario: string;
   public clientes: Array<ClienteModel>;
   public empresaId: number;
-  public documento: DocumentoModel =new DocumentoModel();
+  public documento: DocumentoModel = new DocumentoModel();
   public ordenesList: Array<DocumentoModel>;
   public usuarioId: number;
+  public detallesList: Array<DocumentoDetalleModel> = [];
+  public detalleSelect: DocumentoDetalleModel = new DocumentoDetalleModel();;
   @ViewChild("clientePV") clientePV: ElementRef;
   @ViewChild("placa") placa: ElementRef;
   @ViewChild("descripcionCliente") descripcionCliente: ElementRef;
   @ViewChild("observacion") observacion: ElementRef;
-  
-  
+  @ViewChild("observacion") item: ElementRef;
 
-  constructor(public clienteService: ClienteService,public documentoService: DocumentoService,
+
+  constructor(public clienteService: ClienteService, public documentoService: DocumentoService,
     public documentoDetalleService: DocumentoDetalleService, public calculosService: CalculosService) { }
 
   ngOnInit() {
@@ -38,44 +43,44 @@ export class OtComponent implements OnInit {
   getclientes(empresaId: number) {
     this.clienteService.getClientesByEmpresa(empresaId.toString()).subscribe(res => {
       this.clientes = res;
-      console.log("lista de clientes cargados: "+this.clientes.length);
-    });    
-  }
-
- /* getDetalles() {
-    let empresaId: string = sessionStorage.getItem('empresa_id');
-    this.usuarioService.getByUsuario(this.usuarioBuscar, empresaId, this.rolSelectBuscar).subscribe(res => {    
-      this.usuarioList = res;
+      console.log("lista de clientes cargados: " + this.clientes.length);
     });
   }
-*/
 
-clienteSelectFun(element) {
-  console.log(this.clientes);
+  /* getDetalles() {
+     let empresaId: string = sessionStorage.getItem('empresa_id');
+     this.usuarioService.getByUsuario(this.usuarioBuscar, empresaId, this.rolSelectBuscar).subscribe(res => {    
+       this.usuarioList = res;
+     });
+   }
+ */
+
+  clienteSelectFun(element) {
+    console.log(this.clientes);
     let cliente = this.clientes.find(cliente => cliente.nombre == element.value);
-    if (cliente==undefined) {
+    if (cliente == undefined) {
       alert("El cliente no existe");
       return;
-    } else {  
+    } else {
       console.log(cliente);
-      this.documento.cliente_id = cliente.cliente_id;  
-      if(this.documento.documento_id==""){
+      this.documento.cliente_id = cliente.cliente_id;
+      if (this.documento.documento_id == "") {
         alert("Debe pulsar el boton nuevo documento");
         return;
-      }  
+      }
       this.documentoService.updateDocumento(this.documento).subscribe(res => {
         if (res.code != 200) {
           alert("error actualizando el documento, por favor inicie nuevamente la creación del documento");
           return;
-        }     
+        }
       });
     }
   }
-  
-  nuevaOrden(){
+
+  nuevaOrden() {
     console.log("nueva orden");
     this.limpiar();
-    this.documento.tipo_documento_id  = 11;// SE AGREGA tipo documento =11 orden de trabajo
+    this.documento.tipo_documento_id = 11;// SE AGREGA tipo documento =11 orden de trabajo
     this.documento.fecha_registro = this.calculosService.fechaActual();
     this.documento.fecha_entrega = null;
     this.documento.usuario_id = this.usuarioId;
@@ -89,19 +94,21 @@ clienteSelectFun(element) {
         return;
       }
     });
-    
+
   }
 
-  limpiar(){
+  limpiar() {
     this.documento = new DocumentoModel();
-    
+    this.detallesList = [];
     this.placa.nativeElement.value = "";
     this.clientePV.nativeElement.value = "";
     this.descripcionCliente.nativeElement.value = "";
     this.observacion.nativeElement.value = "";
+    this.item.nativeElement.value = "";
+    
   }
-  agregarObservacion(element){
-    if(this.documento.documento_id==""){
+  agregarObservacion(element) {
+    if (this.documento.documento_id == "") {
       alert("Debe pulsar el boton nuevo documento");
       return;
     }
@@ -110,12 +117,12 @@ clienteSelectFun(element) {
       if (res.code != 200) {
         alert("error actualizando el documento, por favor inicie nuevamente la creación del documento");
         return;
-      }     
+      }
     });
   }
 
-  agregarDescripcionCliente(element){
-    if(this.documento.documento_id==""){
+  agregarDescripcionCliente(element) {
+    if (this.documento.documento_id == "") {
       alert("Debe pulsar el boton nuevo documento");
       return;
     }
@@ -124,12 +131,12 @@ clienteSelectFun(element) {
       if (res.code != 200) {
         alert("error actualizando el documento, por favor inicie nuevamente la creación del documento");
         return;
-      }     
+      }
     });
   }
 
-  agregarPlaca(element){
-    if(this.documento.documento_id==""){
+  agregarPlaca(element) {
+    if (this.documento.documento_id == "") {
       alert("Debe pulsar el boton nuevo documento");
       return;
     }
@@ -139,8 +146,64 @@ clienteSelectFun(element) {
       if (res.code != 200) {
         alert("error actualizando el documento, por favor inicie nuevamente la creación del documento");
         return;
-      }     
+      }
     });
+  }
+
+  agregardetalle(element, cantidad) {
+    var value = element.value;
+
+    if (value == '') {
+      alert("El nombre del repuesto es obligatorio");
+      return;
+    }
+    if (cantidad.value == '') {
+      alert("La cantidad del repuesto es obligatorio");
+      return;
+    }
+    if (this.documento.documento_id == '') {
+      alert("Debe pulsar el boton nuevo documento");
+      return;
+    }
+    if (this.detalleSelect.documento_detalle_id == null) {
+      let docDetalle: DocumentoDetalleModel = new DocumentoDetalleModel();
+      docDetalle.descripcion = value;
+      docDetalle.estado = 1;
+      docDetalle.cantidad = cantidad.value;
+      docDetalle.documento_id = this.documento.documento_id;
+      this.documentoDetalleService.saveDocumentoDetalle(docDetalle).subscribe(res => {
+        if (res.code == 200) {
+          docDetalle.documento_detalle_id = res.documento_detalle_id;
+          this.detallesList.unshift(docDetalle);
+        } else {
+          alert("Error agregando repuesto: " + res.error);
+        }
+      });
+    } else {
+      this.detalleSelect.descripcion = value;
+      this.detalleSelect.cantidad = cantidad.value;
+      //this.documentoDetalleService.saveDocumentoDetalle(this.detalleSelect).subscribe(res => {
+        //if (res.code == 200) {
+          //this.detalleSelect.documento_detalle_id = res.documento_detalle_id;
+          //this.detallesList.unshift(docDetalle);
+        //} else {
+         // alert("Error agregando repuesto: " + res.error);
+       // }
+      //});
+      this.detalleSelect = new DocumentoDetalleModel();
+    }
+
+    element.value = "";
+    cantidad.value = "";
+    this.detalleSelect = new DocumentoDetalleModel();
+    $('#exampleModal').modal('hide');
+  }
+
+  editarItem(articulo, camtidad, item) {
+    camtidad.value = articulo.cantidad;
+    item.value = articulo.descripcion;
+    this.detalleSelect = articulo;
+    $('#exampleModal').modal('show');
   }
 
 }
