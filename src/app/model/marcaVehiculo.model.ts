9796b213a8751b3d724d755567e834586d3e6742
luis miguel gonzalez <luismg8812@hotmail.com>
2020-02-05 10:12:29 -0500

    se agregan filtros de marcas a documentos

---
+++ src/app/model/marcaVehiculo.model.ts
@@ -0,0 +1,10 @@
+export class MarcaVehiculoModel {
+    public marca_vehiculo_id:string;
+    public nombre:string;
+    
+    constructor(){
+        this.marca_vehiculo_id="";
+        this.nombre="";
+       }
+}
+
