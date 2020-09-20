9f2ad37882d864ac549cac16be5d9136fadb89aa
luis miguel gonzalez <luismg8812@hotmail.com>
2020-09-19 19:49:16 -0500

    conexion a surtipulpas

--- src/app/model/parametros.model.ts
+++ src/app/model/parametros.model.ts
@@ -5,7 +5,7 @@ export class ParametrosModel {
     public ambiente:string;
     
     constructor(){
-        this. serverUrl='https://facturacioncloud2019.herokuapp.com';
+        this. serverUrl='https://surtipulpasserver.herokuapp.com';
         //this. serverUrl=AppConfigService.ip+':'+AppConfigService.port;
         //this.ambiente = 'local';
         this.ambiente = 'cloud';
